5d58b2dfd3f8b59f112e058e1170e5b71180372e
nzoba <55888232+nzoba@users.noreply.github.com>
2022-12-11 15:20:47 +0100

    Allow webview zoom

--- app/src/main/java/eu/kanade/tachiyomi/util/system/WebViewUtil.kt
+++ app/src/main/java/eu/kanade/tachiyomi/util/system/WebViewUtil.kt
@@ -38,6 +38,11 @@ fun WebView.setDefaultSettings() {
         useWideViewPort = true
         loadWithOverviewMode = true
         cacheMode = WebSettings.LOAD_DEFAULT
+
+        // Allow zooming
+        setSupportZoom(true)
+        builtInZoomControls = true
+        displayZoomControls = false
     }
 }
 
